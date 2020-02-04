591ec0cd38062bc7525b62ad0b68b8b0fa4af346
Abhinav Sharma <abhi18av@gmail.com>
2020-02-04 12:45:11 +0530

    completed the script draft for the scheme language tasks

--- src/collect_scheme_tasks.py
+++ src/collect_scheme_tasks.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 location = "../_resources/u_acmeism_r_rosettacodedata/Task"
 all_tasks = os.listdir(location)
@@ -13,17 +14,15 @@ for a_task in all_tasks:
         if "Scheme" in os.listdir(new_location):
             all_scheme_tasks.append(new_location)
 
+# print(all_scheme_tasks[0])
+# print(os.listdir(all_scheme_tasks[0]))
 
 
-print(all_scheme_tasks[0])
-print(os.listdir(all_scheme_tasks[0]))
+# TODO copy over python, java, mathematica and javascript folders
 
-"""
-- create new folder 
-- copy over problem description
-- copy over scheme, python, java, mathematica and javascript folders
-
-"""
-
-# for a_scheme_task in all_scheme_tasks:
-
+for a_scheme_task in all_scheme_tasks:
+    folder_name = a_scheme_task.split("/")[-1:][0]
+    new_location = "../scheme_tasks/" + folder_name + "/Scheme"
+    shutil.copytree(a_scheme_task + "/Scheme", new_location)
+    shutil.copy(a_scheme_task + "/00DESCRIPTION", new_location + "/00DESCRIPTION.md")
+    shutil.copy(a_scheme_task + "/README", new_location + "/URL.md")
